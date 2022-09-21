9cd870cbefe5c9a736bda289eaaad98bb1dbb3af
Margaret Kenda <margaret.kenda@yahoo.com>
2022-09-21 06:05:32 +0300

    viewed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
-function App() {
+/*function App() {
   const title = "Mad Max";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
@@ -12,6 +12,17 @@ function App() {
       <MovieCard title={title} genres={genresArr} />
     </div>
   );
-}
+}*/
+// default value props
+function App() {
+  const title = "Mad Max";
+  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
+  return (
+    <div className="App">
+      {/* posterSrc is omitted, so the default value will be used instead */}
+      <MovieCard title={title} genres={genresArr} />
+    </div>
+  );
+}
 export default App;
